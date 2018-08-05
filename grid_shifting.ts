c27e09391d5345dca3aa5b8259be490757f04e26
Spiralfate <sortayne@gmail.com>
2018-08-05 23:52:52 +0400

    Initial commit

---
+++ grid_shifting.ts
@@ -0,0 +1,17 @@
+export class Shifting {
+
+    /*
+    Translates to field markers
+     */
+    remove () {
+        return 'o'
+    }
+
+    add () {
+        return '#'
+    }
+
+    restroy () {
+        return 'x'
+    }
+}
